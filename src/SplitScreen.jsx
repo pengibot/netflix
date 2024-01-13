8a8d6591622ee416c4809ab94bce1fc5811b557f
Benjamin Lambe <pengibot@gmail.com>
2024-01-13 19:18:12 +0000

    Made SplitScreen.jsx have correct snytax and lay out

--- src/SplitScreen.jsx
+++ src/SplitScreen.jsx
@@ -1,15 +1,12 @@
-export const SplitScreen = ({
-    left: Left,
-    right: Right,
-}) => {
-    return (
-        <div>
-            <div>
-                <Left />
-            </div>
-            <div>
-                <Right />
-            </div>
-        </div>
-    )
-}
+export const SplitScreen = ({ left: Left, right: Right }) => {
+  return (
+    <div>
+      <div>
+        <Left />
+      </div>
+      <div>
+        <Right />
+      </div>
+    </div>
+  );
+};

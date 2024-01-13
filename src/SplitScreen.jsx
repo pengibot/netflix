b97e4f8901e0a3ef812568672d985c548305988a
Benjamin Lambe <pengibot@gmail.com>
2024-01-13 19:15:45 +0000

    Added several libraries to deal with Linting and making code look Pretty

---
+++ src/SplitScreen.jsx
@@ -0,0 +1,15 @@
+export const SplitScreen = ({
+    left: Left,
+    right: Right,
+}) => {
+    return (
+        <div>
+            <div>
+                <Left />
+            </div>
+            <div>
+                <Right />
+            </div>
+        </div>
+    )
+}
